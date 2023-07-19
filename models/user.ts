aec6a2346ed3448f7fdf39f676e83a223eb223f4
Alexis Grant <alexis@r2c.dev>
2023-07-19 10:26:00 -0700

    add user secret

--- models/user.ts
+++ models/user.ts
@@ -17,6 +17,7 @@ import challengeUtils = require('../lib/challengeUtils')
 import * as utils from '../lib/utils'
 const security = require('../lib/insecurity')
 const challenges = require('../data/datacache').challenges
+const secret = 'ASDVAJSDUASZGDIADBJS';
 
 class User extends Model<
 InferAttributes<User>,
